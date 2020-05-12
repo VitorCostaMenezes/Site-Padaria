0af6e5d3db9bb1833191d4e837c7d6eaa8f9ef9b
Vitor Costa <vitorcostinha@hotmail.com>
2020-05-11 21:36:15 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+let queue = [
+    {
+      name: "Antonio figueiredo",
+      amount: 3,
+    },
+    {
+      name: "Maria Carolina",
+      amount: 1,
+    },
+    {
+      name: "Jão Vitor",
+      amount: 2,
+    },
+    {
+      name: "Andressa Silva",
+      amount: 27,
+    },
+  ];
+
+  
